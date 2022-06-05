8e29cb89fd0049ac52a3778c5f3316c9036cb621
Denis Lopatin <stalk.sdv@gmail.com>
2022-06-05 22:12:49 +0200

    Initial

---
+++ CartService/CartService/BLL/ManagerCart.cs
@@ -0,0 +1,31 @@
+﻿using CartService.DAL;
+using CartService.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CartService.BLL
+{
+    public class ManagerCart
+    {
+        private readonly GatewayCart _gatewayCart;
+        public ManagerCart()
+        {
+            _gatewayCart = new GatewayCart();
+        }
+        public void AddItem(int cartId, Item item)
+        {
+            _gatewayCart.AddItem(cartId, item);
+        }
+        public void RemoveItem(int cartId, int itemId)
+        {
+            _gatewayCart.RemoveItem(cartId, itemId);
+        }
+        public List<Item> GetItems(int cartId)
+        {
+            return _gatewayCart.GetItems(cartId);
+        }
+    }
+}
---
+++ CartService/CartService/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CartService.BLL;
+
+Console.WriteLine("Hello, World!");
+
+ManagerCart managerCart = new ManagerCart();
+
+int cartId = 1;
+var item1 = new CartService.DAL.Models.Item()
+{
+    Name = "Milk",
+    Price = 2,
+    Image = "milk.png",
+    Quantity = 3
+};
+managerCart.GetItems(cartId).ForEach(v => Console.WriteLine(v.Name));
+managerCart.AddItem(cartId, item1);
+managerCart.GetItems(cartId).ForEach(v => Console.WriteLine(v.Name));
+managerCart.RemoveItem(cartId, item1.Id);
+managerCart.GetItems(cartId).ForEach(v => Console.WriteLine(v.Name));
